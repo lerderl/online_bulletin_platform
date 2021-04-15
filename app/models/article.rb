2c5fe24d7ff47b1eae34e90ef2def96da9e957b9
lerderl <josepholukunle1107@gmail.com>
2021-04-15 11:49:31 +0100

    Enabled file upload for different formats and some styling

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+  mount_uploader :file, FileUploader # Tells rails to use this uploader for this model.
+  validates :name, presence: true # Make sure the owner's name is present.
 end
